6a7d824ffba013eade035bdf67dcf84975ecaba6
joaoescola21 <128557883+joaoescola21@users.noreply.github.com>
2023-11-06 21:55:51 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -10,24 +10,8 @@ document.getElementById("mano").style.display = "block";
 document.getElementById("mano").style.display = "none";
 
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
+
+
